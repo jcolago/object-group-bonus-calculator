0b4a3737a559a5ce5a39db1230f6df76eeb3af12
Abdi <abdullahia0215@gmail.com>
2023-08-29 20:23:22 -0500

    added comments describing the different parts

--- client.js
+++ client.js
@@ -49,8 +49,10 @@ console.log('array of employee data: ',  employees );
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus(employee) {
+  // Initialize bonusPercentage variable to 0
   let bonusPercentage = 0;
 
+  // Check the employee's reviewRating and assign the appropriate bonusPercentage
   if (employee.reviewRating <= 2) {
     bonusPercentage = 0;
   } else if (employee.reviewRating === 3) {
@@ -61,24 +63,28 @@ function calculateIndividualEmployeeBonus(employee) {
     bonusPercentage = 0.1;
   }
 
+  // Add an additional bonus if the employeeNumber has 4 digits
   if (employee.employeeNumber.length === 4) {
     bonusPercentage += 0.05;
   }
 
+  // Adjust the bonusPercentage if the annualSalary is above $65,000
   if (employee.annualSalary > 65000) {
     bonusPercentage -= 0.01;
   }
 
+  // Ensure the bonusPercentage is within the acceptable range (0 to 13%)
   if (bonusPercentage > 0.13) {
     bonusPercentage = 0.13;
   } else if (bonusPercentage < 0) {
     bonusPercentage = 0;
   }
 
+  // Calculate the bonusAmount and totalCompensation
   const bonusAmount = Math.round(employee.annualSalary * bonusPercentage);
   const totalCompensation = Number(employee.annualSalary) + bonusAmount;
 
-
+  // Return an object with the employee's name, bonusPercentage, totalCompensation, and bonusAmount
   return {
     name: employee.name,
     bonusPercentage: bonusPercentage,
@@ -87,13 +93,16 @@ function calculateIndividualEmployeeBonus(employee) {
   };
 }
 
+// Loop over the employees array and calculate the bonus data for each employee
 for (let i = 0; i < employees.length; i++) {
   const employee = employees[i];
+
+  // Calculate the bonus data for the current employee
   const bonusData = calculateIndividualEmployeeBonus(employee);
-  
+
+  // Print the employee's name and their bonus data
   console.log('Employee:', employee.name);
   console.log('Bonus Data:', bonusData);
-
 }
 
   
